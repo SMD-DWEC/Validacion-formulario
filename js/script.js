4ded358a5eff89a1bf4b3b0144d30efbe56d46ad
usasergio <usasergio2@gmail.com>
2021-10-26 22:18:05 +0200

    Regex telefono, comprobaciones Asturiano y desplegable.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /**
  * Script para validar un formulario
-    @author: Sergio Matamoros dELGADO
+    @author: Sergio Matamoros Delgado
     @license GPL v3 2021
 **/
 
@@ -8,52 +8,9 @@
 
 
 window.onload = iniciar;
+window.onclick = clicks;
 
 function iniciar() {
-    if(document.getElementById("sComunidad").click) {
-        let container = document.querySelector("form");
-
-        let row = document.createElement("div");
-        row.classList.add("row");
-
-
-        let divTexto = document.createElement("div");
-        divTexto.classList.add("col-25");
-
-        let label = document.createElement("label");
-        label.textContent = "Provincia";
-
-        //Select y options
-        let divSelect = document.createElement("div");
-        divSelect.classList.add("col-75");
-        let select = document.createElement("select");
-        select.id = "sProvincia";
-
-        let oCaceres = document.createElement("option");
-        oCaceres.setAttribute("value","Caceres");
-        oCaceres.textContent = "Caceres";
-
-        let oBadajoz = document.createElement("option");
-        oBadajoz.setAttribute("value","Badajoz");
-        oBadajoz.textContent = "Badajoz";
-
-       
-
-
-        row.appendChild(divTexto);
-        divTexto.appendChild(label);
-
-        //Select
-        row.appendChild(divSelect);
-        divSelect.appendChild(select);
-        select.appendChild(oBadajoz);
-        select.appendChild(oCaceres);
-
-
-
-        container.appendChild(row);
-
-    }
     let form = document.forms[0];
     form.onsubmit = validar;
 }
@@ -61,25 +18,78 @@ function iniciar() {
 function validar(event) {
     
     //event.preventDefault();
-    console.log("valida");
 
 
     //Comprobamos que haya aceptado la P.P
     if(!document.getElementById("iPolitica").checked)
         return false;
 
-    if(document.getElementById("iNombre").lenght <2)
+    //Comprobamos que el nombre tenga más de 2 letras.
+    if(document.getElementById("iNombre").value.length <2) {
+        document.getElementById("iNombre").focus();
         return false;
 
-    //if(document.getElementById("sComunidad"))
-    //{
-        
-
-
-
-
-    //}
+    }
+    //Si eres Asturiano y te gusta el brócoli, pa fuera.
+    if(document.getElementById("rSi").checked && document.getElementById("sComunidad").options[2])
+        return false;
     
+    //Comprobación regex del teléfono.
+    if(!document.getElementById("iTelefono").value.match("[0-9]{9}"))
+        return false;
+    
+    console.log("valida");
 
     //return false; <- le indico al navegador que no pase nada
+}
+
+//Función que controla a que elemento se le ha hecho click.
+function clicks(event) {
+    if(!(document.getElementById("sProvincia")) && event.target.id == "sComunidad" && event.target.value==11) {
+        provincia();
+    } else if(document.getElementById("sProvincia") && event.target.id == "sComunidad" && event.target.value != 11)
+        document.querySelectorAll(".row")[7].remove();
+}
+
+/**
+ * Función que despliega un select al seleccionar la CCAA de Extremadura.
+ */
+function provincia() {
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    let divTexto = document.createElement("div");
+    divTexto.classList.add("col-25");
+
+    let label = document.createElement("label");
+    label.textContent = "Provincia";
+
+    //Select y options
+    let divSelect = document.createElement("div");
+    divSelect.classList.add("col-75");
+    let select = document.createElement("select");
+    select.id = "sProvincia";
+
+    let oCaceres = document.createElement("option");
+    oCaceres.setAttribute("value","Caceres");
+    oCaceres.textContent = "Caceres";
+
+    let oBadajoz = document.createElement("option");
+    oBadajoz.setAttribute("value","Badajoz");
+    oBadajoz.textContent = "Badajoz";
+
+   
+
+
+    row.appendChild(divTexto);
+    divTexto.appendChild(label);
+
+    //Select
+    row.appendChild(divSelect);
+    divSelect.appendChild(select);
+    select.appendChild(oBadajoz);
+    select.appendChild(oCaceres);
+
+    //Ponemos el select en la posición número 7, que es justo después del select de la CCAA
+    document.querySelector("form").insertBefore(row, document.querySelectorAll("form .row")[7]);
 }
